82a2e1d7569115e0f6cd6b937e0c67bb531c627e
cph101 <cph101@icloud.com>
2024-05-19 17:50:00 +0000

    Working

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const template = '<div class="cell">' +
-	'<div class="radio w-100 h-100">' +
-		'<input id="$2" name="capeRadioSelect" type="radio" class="btn-radio">' +
-		'<label for="$3" class="radio-selection">' +
-			'<div class="mb-3">' +
-				'<div class="d-inline-block">$0</div>' +
-			'</div>' +
-			'<div class="text-truncate">' +
-				'<span class="radio-icon"></span>' +
-				'<span class="radio-label">$1</span>' +
-			'</div>' +
-		'</label>' +
-	'</div>' +
-'</div>',
+const template = `<div class="cell">
+<div class="radio w-100 h-100">
+	<input id="$2" name="capeRadioSelect" type="radio" class="btn-radio">
+	<label for="$3" class="radio-selection">
+		<div class="mb-3">
+			<div class="d-inline-block">$0</div>
+		</div>
+		<div class="text-truncate">
+			<span class="radio-icon"></span>
+			<span class="radio-label">$1</span>
+		</div>
+	</label>
+</div></div>`,
 	capes = [
 		['No cape', 'No cape'],
 		['15 Year Anniversary'],
@@ -41,9 +40,9 @@ const template = '<div class="cell">' +
 		['Valentine'],
 		['Vanilla']
 	],
-	img = '<div class="position-relative" style="height: 128px; width: 80px;">' +
-		'<img src="$4" alt="Cape image">' +
-	'</div>',
+	img = `<div class="position-relative" style="height: 128px; width: 80px;">
+		<img src="$4" alt="Cape image">
+	</div>`,
 	can = '<canvas width="80" height="128" data-image="$5"></canvas>';
 
 function setImg(c) {
@@ -58,16 +57,24 @@ function setImg(c) {
 
 window.addEventListener('load', function() {
 	const row = document.getElementById('row');
-	for (var i = 0; i < capes.length; i++) {
-		const cape = capes[i],
+	capes.forEach((cape, i) => {
 			name = cape[0],
 			image = cape[1] || name;
 		row.innerHTML += template.replace('$0', (i==0 ? img : can)).replace('$1', name).replace('$2', i).replace('$3', i).replace('$4', 'img/' + image + '.png').replace('$5', image);
-	}
+	});
+	document.getElementById('0').checked = true;
 	var c = document.getElementsByTagName('canvas');
-	for (var i=0; i < c.length; i++) {
-		setImg(c[i]);
+	for (canvas of c) {
+		setImg(canvas);
 	}
+	this.window.setTimeout(() => {
+		html2canvas(document.getElementById("main")).then((canvas) => {
+			var link = document.createElement('a');
+  			link.download = "MinecraftCapesSelection.png";
+  			link.href = canvas.toDataURL("image/png");
+  			link.click();
+		});
+	}, 5000);
 }, false);
 
 
